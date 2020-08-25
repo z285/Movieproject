ba29f47b016ed6c63592eecc163600568a898dcc
Ashita <ashita.kum@gmail.com>
2020-08-25 20:14:06 +0700

    Format code

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import BootstrapVue from 'bootstrap-vue'
 import VueCookies from 'vue-cookies'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
- 
+
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
